f4c32eacad1795642e2e678fcca3d8b534140db6
Piyush Pamnani <piyushpamnani46@gmail.com>
2023-08-10 22:53:34 +0530

    Deployed Server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://pomodro-timer.onrender.com",
 });
 
 export const signIn = (formData) => API.post("/users/signin", formData);
