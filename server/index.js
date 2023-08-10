1ae834cccaf2916afc0791b540ad67a2cee713e4
Piyush Pamnani <piyushpamnani46@gmail.com>
2023-08-10 22:41:46 +0530

    Server PORT

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 dotenv.config();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
